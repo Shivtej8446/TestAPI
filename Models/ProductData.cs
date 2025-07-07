40e24112b92fa2b2541f1cb33515aa44f23e08e7
Shivtej8446 <124593358+Shivtej8446@users.noreply.github.com>
2025-07-07 12:04:31 +0530

    Add files via upload

---
+++ Models/ProductData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class ProductData
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public decimal QuantityInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal CostPrice { get; set; }
+    }
+}
